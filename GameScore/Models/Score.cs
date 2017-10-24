7a46988d0a1955f859511417b86eae8f69c16fe0
joderrick1022 <joderrick1022>
2017-10-24 15:14:21 -0400

    model setup

---
+++ GameScore/Models/Score.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GameScore.Models
+{
+    public class Score 
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Points { get; set; }
+        public string Team { get; set; }
+
+    
+    }
+   
+
+    
+}
